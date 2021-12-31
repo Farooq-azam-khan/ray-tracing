17557a869a8471cb5dcbb7612711c5b64af03930
Farooq <farooq1.khan@ryerson.ca>
2021-12-31 17:43:49 -0500

    done ch1

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,26 @@
 fn draw_image() {
-    // Image 
-    let aspect_ratio = 16.0 / 9.0; 
+    // Image
+    let image_width: u32 = 256;
+    let image_height: u32 = 256;
 
-    let image_width = 400; 
-    let image_height = (image_width as f64 / aspect_ratio as f64) as i64; 
-
-    // Camera
-    let viewport_height = 2.0; 
-    let viewport_width = aspect_ratio * viewport_height; 
-    let focal_length = 1.0; 
-
-    let origin = Vec3::new(0.0,0.0,0.0); 
-    let horizontal = Vec3::new(viewport_width, 0.0,0.0); 
-    let vertical = Vec3::new(0.0, viewport_height, 0.0); 
-    let lower_left_corner = origin - horizontal/2.0 - vertical/2.0 - Vec3::new(0.0, 0.0, focal_length); 
-
-
-    // Renger 
-    println!("P3\n{} {}\n255", image_width, image_height); 
-
+    // Render
+    println!("P3\n{} {}\n255", image_width, image_height);
     for j in (0..image_height).rev() {
-        eprintln!("Scanlines remaining: {}", j); 
+        eprintln!("Scanlines Remaining: {}", j);
         for i in 0..image_width {
-            let u = (i as f64) / ((image_width - 1) as f64); 
-            let v = (j as f64) / ((image_height -1) as f64); 
+            let r = (i as f32) / (image_width as f32 - 1.0);
+            let g = (j as f32) / (image_height as f32 - 1.0);
+            let b = 0.25;
 
-            let ray = Ray::new(origin, lower_left_corner + u*horizontal + v*vertical - origin); 
-            let ray_c = ray_color(ray); 
-            write_color(ray_c); 
-
+            let ir: u32 = (255.999 * r) as u32;
+            let ig: u32 = (255.999 * g) as u32;
+            let ib: u32 = (255.999 * b) as u32;
+            println!("{} {} {}", ir, ig, ib);
         }
     }
-    eprintln!("Done"); 
-
-
+    eprintln!("Done!");
 }
 
 fn main() {
-  
     draw_image();
-
-
 }
-
