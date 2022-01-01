49584851711e4433fc8d005e4960d91bcf516ec1
Farooq <farooq1.khan@ryerson.ca>
2021-12-31 20:07:00 -0500

    adding a sphere

---
+++ src/sphere.rs
@@ -0,0 +1,6 @@
+/* Equation of a sphere: x^2 + y^2 + z^2 = R^2
+given a point (a,b,c)
+if a^2 + b^2 + c^2 = R^2 then the point (a,b,c) is on the sphere
+else if a^2 + b^2 + c^2 < R^2 point (a,b,c) is inside the sphere
+else point is outside the sphere (i.e. a^2 + b^2 + c^2 > R^2)
+*/
