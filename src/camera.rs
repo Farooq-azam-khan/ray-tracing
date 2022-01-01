bb6a7534fea050e1714ab99c480dc0d2d4ad556a
Farooq <farooq1.khan@ryerson.ca>
2022-01-01 10:14:44 -0500

    did antialiasing by averaging all the pixels given a sample

---
+++ src/camera.rs
@@ -0,0 +1,43 @@
+use crate::ray::Ray;
+use crate::vec3::{Point3, Vec3};
+
+pub struct Camera {
+    aspect_ratio: f64,
+    viewport_height: f64,
+    focal_length: f64,
+    origin: Point3,
+    horizontal: Vec3,
+    vertical: Vec3,
+}
+
+impl Camera {
+    pub fn new(aspect_ratio: f64, viewport_height: f64, focal_length: f64) -> Camera {
+        let viewport_width = aspect_ratio * viewport_height;
+        Camera {
+            aspect_ratio,
+            viewport_height,
+            focal_length,
+            origin: Point3::new(0.0, 0.0, 0.0),
+            horizontal: Vec3::new(viewport_width, 0.0, 0.0),
+            vertical: Vec3::new(0.0, viewport_height, 0.0),
+        }
+    }
+    pub fn get_viewport_height(&self) -> f64 {
+        self.viewport_height
+    }
+    pub fn get_viewport_width(&self) -> f64 {
+        self.aspect_ratio * self.viewport_height
+    }
+    pub fn get_lower_left_corner(&self) -> Vec3 {
+        self.origin
+            - self.horizontal / 2.0
+            - self.vertical / 2.0
+            - Vec3::new(0.0, 0.0, self.focal_length)
+    }
+    pub fn get_ray(&self, u: f64, v: f64) -> Ray {
+        Ray::new(
+            self.origin,
+            self.get_lower_left_corner() + u * self.horizontal + v * self.vertical - self.origin,
+        )
+    }
+}
--- src/colour.rs
+++ src/colour.rs
@@ -1,8 +1,20 @@
-use crate::vec3; //::Colour;
+use crate::utils::clamp;
+use crate::vec3::Colour;
 
-pub fn write_colour(colour: vec3::Colour) {
-    let ir: u32 = (255.999 * colour.r()) as u32;
-    let ig: u32 = (255.999 * colour.g()) as u32;
-    let ib: u32 = (255.999 * colour.b()) as u32;
+pub fn write_colour(colour: Colour, samples_per_pixel: u32) {
+    let mut r = colour.r();
+    let mut g = colour.g();
+    let mut b = colour.b();
+
+    // divide colour by number of samples
+    let scale = 1.0 / samples_per_pixel as f64;
+    r *= scale;
+    g *= scale;
+    b *= scale;
+
+    let ir: u32 = (256.0 * clamp(r, 0.0, 0.999)) as u32;
+    let ig: u32 = (256.0 * clamp(g, 0.0, 0.999)) as u32;
+    let ib: u32 = (256.0 * clamp(b, 0.0, 0.999)) as u32;
+
     println!("{} {} {}", ir, ig, ib);
 }
